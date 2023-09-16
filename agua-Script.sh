c05a7c43195f65209a61a1ba58ad04eee8e005ae
Tr1sme3gist0 <ipbassart@proton.me>
2023-09-16 21:12:01 +0100

    done

--- agua-Script.sh
+++ agua-Script.sh
@@ -5,18 +5,6 @@ sudo iptables -F
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -P OUTPUT DROP
-#CHECK PORTS
-sudo ss -tuln  
-#BLEACHBIT
-read -p "Crash? (yes/no): " choice
-if [[ $choice == "yes" ]]; then
-        sudo bleachbit
-elif [[ $choice == "no" ]]; then
-    echo "Operation canceled."
-fi
-#PROCESS
-htop
-ps -auxwf
 #IPTABLES ACCEPT
 execute_flag="no"               #FLAG OPTION -x
 while [[ $# -gt 0 ]]; do
@@ -36,4 +24,17 @@ if [ "$execute_flag" == "yes" ]; then
     sudo iptables -P INPUT ACCEPT
     sudo iptables -P FORWARD ACCEPT
     sudo iptables -P OUTPUT ACCEPT
+    exit
 fi
+#CHECK PORTS
+sudo ss -tuln  
+#BLEACHBIT
+read -p "Crash? (yes/no): " choice
+if [[ $choice == "yes" ]]; then
+        sudo bleachbit
+elif [[ $choice == "no" ]]; then
+    echo "Operation canceled."
+fi
+#PROCESS
+htop
+ps -auxwf
