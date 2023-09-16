5593a596622da43e5da97fee00d6ba9e08be416c
Tr1sme3gist0 <ipbassart@proton.me>
2023-09-16 19:59:13 +0100

    changes

--- agua-Script.sh
+++ agua-Script.sh
@@ -1,7 +1,36 @@
 !#/bin/bash
+
+
 #IPTABLES TO DROP
 sudo iptables -F
 sudo iptables -P INPUT DROP
 sudo iptables -P FORWARD DROP
 sudo iptables -P OUTPUT DROP
-
+#MONITORING
+sudo ss -tuln           
+htop
+ps -auxwf
+wireshark&
+#BLEACHBIT
+read -p "Do you want to run 'sudo bleachbit'? (yes/no): " choice
+if [[ $choice == "yes" ]]; then
+        sudo bleachbit
+elif [[ $choice == "no" ]]; then
+    echo "Operation canceled."
+#IPTABLES ACCEPT
+execute_flag="no"               #FLAG OPTION -x
+while getopts "x" opt; do
+  case $opt in
+    x)
+      execute_flag="yes"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+if [ "$execute_flag" == "yes" ]; then
+  echo "Executing commands because -x option was provided."
+  sudo iptables -P INPUT ACCEPT
+  sudo iptables -P FORWARD ACCEPT
+  sudo iptables -P OUTPUT ACCEPT
