572d673de7b6fa4098d6a89646665f68c4461c20
Tr1sme3gist0 <ipbassart@proton.me>
2023-09-16 18:47:28 +0100

    changes

--- agua-Script.sh
+++ agua-Script.sh
@@ -1,4 +1,6 @@
+!#/bin/bash
 
-There may be situations where you may feel like you need to shut down ports, and run wireshark. See a tcpdump log and even wipe out your RAM entirely. Before deploying snort.
-And you need to do that quick and you need to do that all at the same time.
-That what agua does
+sudo iptables -F
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT DROP
