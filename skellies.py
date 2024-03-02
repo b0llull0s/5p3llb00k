b0adb3ebb82175ed602844f6b94ee08c51524290
b0llull0s <128314429+b0llull0s@users.noreply.github.com>
2024-03-02 20:32:43 +0000

    Update skellies.py
    Add it loop and functions

--- skellies.py
+++ skellies.py
@@ -1,28 +1,35 @@
-## Hex generator ##
 import random
 import string
+## Variables ##
 hex_characters='0123456789abcdef'
-## User length ##
-hexname_length=13  
-## User name ##
-hexname=''
-for _ in range(hexname_length):
-    hexname+=random.choice(hex_characters)
-## email ##
 email_list=["gmail","yahoo","outlook","proton"]
-print("Choose an email from this list:\n",email_list)
-email=input("Email:")
-if email in email_list:
+## Funtions ##
+def generate_hex_email(email):
+    hexname_length=13
+    hexname=''
+    for _ in range(hexname_length):
+        hexname+=random.choice(hex_characters)
     if email=="proton":
-        email=hexname+"@"+email+".me"
+        return hexname+"@"+email+".me"
     else:
-        email=hexname+"@"+email+".com"
+        return hexname+"@"+email+".com"
+
+def generate_password(password_length):
+    all_characters=string.ascii_letters+string.digits+string.punctuation
+    return ''.join(random.choice(all_characters)for _ in range(password_length))
+## Loop ##
+num_combinations=int(input("How many skelletons do you want? "))
+if num_combinations<1:
+    print("Invalid number of combinations.")
 else:
-    print("Invalid email provider chosen.") 
-print("Random Hex email address:",email)
-## Password String ##
-all_characters=string.ascii_letters+string.digits+string.punctuation
-## Password length ##
-password_length=int(input("Enter the desired length of the password: "))
-password=''.join(random.choice(all_characters) for _ in range(password_length))
-print("Generated Password:",password)
+    for _ in range(num_combinations):
+        print("Choose an email from this list:\n",email_list)
+        email=input("Email: ").lower()
+        if email in email_list:
+            email=generate_hex_email(email)
+            print("Random Hex email address:",email)
+            password_length=int(input("Desired length of the password? "))
+            password=generate_password(password_length)
+            print("Generated Password:", password)
+        else:
+            print("Invalid email provider chosen.")
