7d761eab6b25924faea92586b876bb0d8286c19c
b0llull0s <128314429+b0llull0s@users.noreply.github.com>
2024-06-17 17:36:06 +0100

    Add files via upload

--- st1ng.sh
+++ st1ng.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# St1ng - By b0llull0s
 echo "      .. ..',..''''','',,,,,,,,,,',,,,'.,,',,,,,,;,,,,,,,,,,,,'.',,'.  ..."
 echo "       .   ..'...'.,'''''',,;;;,,,,;,,''.',';,,,;;;;;,,,,,,'......,'.    .."
 echo "            .'..'..'',,;,;;;;;;;,;;;,,.';;;',;;;;:;;;;;;;,,...''';,.   ."
@@ -23,23 +22,28 @@ echo "        ...   . ..       .,,..''                .'.,;,'..    ...  ...,."
 echo "               ............;,.,,.       ..      ''',,....  ...'.....'.."
 echo "                    .........'''.      .'.     ..''.... ........   .."
 echo "                     ...''.';:;;;;'...;:;,'....;':;,','  .;,..                  .."
-# Display function
+# St1ng - By b0llull0s
+
+# Display Function
 usage() {
-    echo "Usage: $0 -u URL -d IP_ADDRESS -p PORT -f FILE_NAME [-c] [-w]"
+    echo "Usage: $0 -u URL -d IP_ADDRESS -p PORT -f FILE_NAME [-c] [-w] [-l]"
     echo "  -u URL          : URL to download the file from"
-    echo "  -d IP_ADDRESS   : IP address to send the file to"
-    echo "  -p PORT         : Port number to send the file to"
+    echo "  -d IP_ADDRESS   : IP address to send the file to (ignored in listening mode)"
+    echo "  -p PORT         : Port number to send or listen on"
     echo "  -f FILE_NAME    : Name of the file to save and send"
     echo "  -c              : Use curl to download the file (default is wget)"
+    echo "  -w              : Use wget to download the file (default if neither -c nor -w is specified)"
+    echo "  -l              : Listen for inbound connections instead of sending outbound"
     exit 1
 }
 
 # Variables
 use_curl=false
 use_wget=false
+listen_mode=false
 
-# Arguments
-while getopts "u:d:p:f:cw" opt; do
+# Aguments
+while getopts "u:d:p:f:cwl" opt; do
     case ${opt} in
         u ) url=$OPTARG ;;
         d ) ip_address=$OPTARG ;;
@@ -47,12 +51,13 @@ while getopts "u:d:p:f:cw" opt; do
         f ) file_name=$OPTARG ;;
         c ) use_curl=true ;;
         w ) use_wget=true ;;
+        l ) listen_mode=true ;;
         * ) usage ;;
     esac
 done
 
-# Validate arguments
-if [ -z "$url" ] || [ -z "$ip_address" ] || [ -z "$port" ] || [ -z "$file_name" ]; then
+# Validation
+if [ -z "$url" ] || [ -z "$port" ] || [ -z "$file_name" ]; then
     usage
 fi
 
@@ -60,7 +65,7 @@ if [ "$use_curl" = false ] && [ "$use_wget" = false ]; then
     use_wget=true
 fi
 
-# Download loop
+# Downloading loop
 if [ "$use_curl" = true ]; then
     echo "Downloading file using curl..."
     curl -o "$file_name" "$url"
@@ -78,11 +83,23 @@ elif [ "$use_wget" = true ]; then
 fi
 
 # Ncat loop
-echo "Sending file using ncat..."
-ncat --send-only "$ip_address" "$port" < "$file_name"
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to send file using ncat."
-    exit 1
+if [ "$listen_mode" = true ]; then
+    echo "Listening for inbound connections using ncat..."
+    sudo ncat -l -p "$port" --send-only < "$file_name"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to listen for inbound connections using ncat."
+        exit 1
+    fi
+else
+    if [ -z "$ip_address" ]; then
+        usage
+    fi
+    echo "Sending file using ncat..."
+    ncat --send-only "$ip_address" "$port" < "$file_name"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to send file using ncat."
+        exit 1
+    fi
 fi
 
-echo "File sent successfully."
+echo "Operation completed successfully."
